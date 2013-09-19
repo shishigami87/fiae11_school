f0bd4076fd7b5ba36350409afdcfeaf9c67fd5e4
j0ch3 <j-hirt@web.de>
2013-09-19 11:51:33 +0200

    Just a troll project

---
+++ TrollProject/src/karteikasten/Main.java
@@ -0,0 +1,30 @@
+package karteikasten;
+
+public class Main {
+
+	public static class InnerMain {
+		static{
+			// Hier wichtigen Code einfügen
+		}
+		{
+			// Hier wichtigeren Code einfügen
+		}
+		public static class InnererClass {
+			public static void macheWas() {
+				//Hier den wichtigesten Code einfügen
+			}
+
+			static {
+				{
+					{
+						macheWas();
+					}
+				}
+			}
+			{
+				macheWas();
+			}
+
+		}
+	}
+}
