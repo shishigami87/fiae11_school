52fefc6ae90530bac1803242fb435997a6a7f819
shishigami87 <shishigami@gotmail.de>
2013-09-19 08:08:11 +0200

    Added FileReader snippet

--- common-snippets/src/org/shishigami/common/snippets/Start.java
+++ common-snippets/src/org/shishigami/common/snippets/Start.java
@@ -3,17 +3,22 @@ package org.shishigami.common.snippets;
 import java.io.IOException;
 
 import org.shishigami.common.snippets.file.PropertiesReader;
+import org.shishigami.common.snippets.file.ReadFile;
 
 public class Start {
 	
 	public static void main(String[] args) {
-		PropertiesReader propertiesReader = new PropertiesReader();
-		
 		try {
+			System.out.println("[INFO] Testing PropertiesReader\n");
+			PropertiesReader propertiesReader = new PropertiesReader();
 			System.out.println(propertiesReader.createPersonFromProperties());
 			System.out.println(propertiesReader.createModifiedPersonFromProperties());
+			
+			System.out.println("\n[INFO] Testing FileReader\n");
+			ReadFile readFile = new ReadFile();
+			readFile.printFileContent();
 		} catch (IOException e) {
-			System.out.println("File error! \n \n" + e);
+			e.printStackTrace();
 		}
 	}
 
---
+++ common-snippets/src/org/shishigami/common/snippets/file/ReadFile.java
@@ -0,0 +1,24 @@
+package org.shishigami.common.snippets.file;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ReadFile {
+
+	public void printFileContent() throws IOException {
+		File file = new File("resources/test.txt");
+		InputStream inputStream = new FileInputStream(file);
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		
+		String line = null;
+		while ((line = bufferedReader.readLine()) != null) {
+			System.out.println(line);
+		}
+	}
+
+}
