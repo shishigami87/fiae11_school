0cd071b3b35599690dd476416765f07afcad4ece
j0ch3 <j-hirt@web.de>
2013-09-27 09:17:33 +0200

    test

--- jahr3-pattern-flyweight/src/org/bbst1/fiae11/lf6/pattern/flyweight/wort/CountedWord.java
+++ jahr3-pattern-flyweight/src/org/bbst1/fiae11/lf6/pattern/flyweight/wort/CountedWord.java
@@ -5,38 +5,37 @@ import java.util.Set;
 
 public class CountedWord {
 
-	private final String wort;
-	private int anzahl;
+    private final String wort;
+    private int anzahl;
 
-	private static Set<CountedWord> worte = new HashSet<CountedWord>();
+    private static Set<CountedWord> worte = new HashSet<CountedWord>();
 
-	public static boolean countWord(String word) {
-		for (CountedWord w : worte) {
-			if (w.wort.equals(word)) {
-				w.anzahl++;
-				return true;
-			}
-		}
-		new CountedWord(word);
-		return false;
-	}
+    public static boolean countWord(String word) {
+        for (CountedWord w : worte) {
+            if (w.wort.equals(word)) {
+                w.anzahl++;
+                return true;
+            }
+        }
+        new CountedWord(word);
+        return false;
+    }
 
-	private CountedWord(String wort) {
-		worte.add(this);
-		this.wort = wort;
-		this.anzahl = 1;
-	}
+    private CountedWord(String wort) {
+        worte.add(this);
+        this.wort = wort;
+        this.anzahl = 1;
+    }
 
-	public static Set<CountedWord> getWorte() {
-		return worte;
-	}
+    public static Set<CountedWord> getWorte() {
+        return worte;
+    }
 
-	public String getWort() {
-		return wort;
-	}
+    public String getWort() {
+        return wort;
+    }
 
-	public int getAnzahl() {
-		return anzahl;
-	}
-
+    public int getAnzahl() {
+        return anzahl;
+    }
 }
